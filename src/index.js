2632210dbe672cffb4ec6b8a337797ae0913caf8
pavel.vasilyev <pavel.vasilyev@moneyman.ru>
2021-05-13 17:24:04 +0300

    feat: added modalCallback function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-// setInterval(countTimer, 1000, '22 April 2021');
-// countTimer('22 April 2021');
-// import countTimer from './modules/countTimer';
-// import toggleMenu from './modules/toggleMenu';
-// import togglePopUp from './modules/togglePopup';
-// import tabs from './modules/tabs';
-// import slider from './modules/slider';
-// import setCommandImg from './modules/setCommandImg';
-// import validateInputs from './modules/validateInputs';
-// import calc from './modules/calc';
-// import sendForm from './modules/sendForm';
+'use strict'
+import modalCallback from './modules/modalCallback';
 
-
-// countTimer('31 May 2021');
-// toggleMenu();
-// togglePopUp();
-// tabs();
-// slider();
-// setCommandImg();
-// validateInputs();
-// calc();
-// sendForm();
+modalCallback();
---
+++ src/modules/modalCallback.js
@@ -0,0 +1,26 @@
+const modalCallback = () => {
+    const modalCallback = document.querySelector('.modal-callback');
+    const modalOverlay = document.querySelector('.modal-overlay');
+
+
+
+    const handlerCallback = (event) => {
+		const target = event.target;
+
+		const displayCallback = () => {
+			modalCallback.classList.toggle('_active');
+			modalOverlay.classList.toggle('_active');
+		};
+
+		if (target.closest('.callback-btn') ||
+			(!target.closest('.modal-callback') &&
+			modalCallback.classList.contains('_active')) || (target.closest('.modal-close'))) {
+			displayCallback();
+		}
+	};
+
+	document.body.addEventListener('click', handlerCallback);
+
+};
+
+export default modalCallback;
