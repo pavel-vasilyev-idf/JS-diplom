e4b1094a3dca59ee125951d4de3729cd0ede243b
pavel.vasilyev <pavel.vasilyev@moneyman.ru>
2021-05-14 23:59:44 +0300

    feat: check form

--- src/modules/scrollUp.js
+++ src/modules/scrollUp.js
@@ -1,7 +1,26 @@
 const scrollUp = () => {
 
     const upBtn = document.querySelector('.up');
-    console.log(screenTop);
+    
+    let sec = document.querySelector('.services-section')
+    
+    console.log(sec.scrollHeight);
+    console.log(sec.scrollHeight);
+
+    let a;
+
+    document.body.addEventListener('scroll', () => {
+
+        a = document.documentElement.scrollTop;
+
+        // if (document.documentElement.scrollTop >= sec.scrollHeight) {
+        //     upBtn.classList.add('up_active')
+        // } else if(document.documentElement.scrollTop < sec.scrollHeight) {
+        //     upBtn.classList.remove('up_active')
+        // }
+    })
+
+
 
 }
 
